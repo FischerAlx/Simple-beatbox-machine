f1128df369c25eb323131db630cc33a648f3ce81
FischerAlx <fisheralx@icloud.com>
2022-11-27 22:07:35 +0100

    Initial Commit

---
+++ Simple beatbox machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple beatbox machine
+//
+//  Created by Александр Фишер on 27.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
