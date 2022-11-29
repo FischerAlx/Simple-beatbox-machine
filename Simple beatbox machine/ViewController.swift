1670fbcbc824b7ca08ee4b1a88c693d1f50b775f
FischerAlx <fisheralx@icloud.com>
2022-11-29 15:01:48 +0100

    add a sound(for now n.w.)

--- Simple beatbox machine/ViewController.swift
+++ Simple beatbox machine/ViewController.swift
@@ -4,19 +4,38 @@
 //
 //  Created by Александр Фишер on 27.11.22.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func keyKickPressed(_ sender: UIButton) {
-        print("Im pressed")
+        playSound()
         
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "Kick", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
 }
