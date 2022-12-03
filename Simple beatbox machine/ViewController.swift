00fb45e2cf3f6f5266227ec9548e3d06b72c189f
FischerAlx <fisheralx@icloud.com>
2022-12-03 15:12:18 +0100

    reformat code

--- Simple beatbox machine/ViewController.swift
+++ Simple beatbox machine/ViewController.swift
@@ -8,20 +8,20 @@ import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyKickPressed(_ sender: UIButton) {
         
         
         UIView.animate(withDuration: 0.3) {
-                sender.alpha = 0.5
-            }
-
+            sender.alpha = 0.5
+        }
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 ) {
             UIView.animate(withDuration: 0.1) {
                 sender.alpha = 1.0
